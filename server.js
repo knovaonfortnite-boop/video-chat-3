72b6093bc6445e6637f663ceeaa62ef5c11d4268
knovaonfortnite-boop <knovaonfortnite@gmail.com>
2025-10-16 04:39:45 -0500

    Refactor server.js to simplify static file serving
    Removed comments and cleaned up server.js.

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const WebSocket = require('ws');
 
-// Serve static files from the public folder
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve index.html for all routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// Listen on Render's port
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT
