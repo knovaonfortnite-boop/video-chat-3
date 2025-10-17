77bde75c8a0fdc6514651844570757c8166bbf0e
knovaonfortnite-boop <knovaonfortnite@gmail.com>
2025-10-16 21:58:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,31 @@
-// server.js
 const express = require("express");
 const http = require("http");
-const WebSocket = require("ws");
+const { Server } = require("socket.io");
 const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let clients = new Map();
+let users = {};
 
-wss.on("connection", (ws) => {
-  const id = Math.random().toString(36).substr(2, 9);
-  clients.set(id, ws);
-  console.log(`Client ${id} connected`);
+io.on("connection", (socket) => {
+  users[socket.id] = "Anonymous";
 
-  ws.send(JSON.stringify({ type: "welcome", id, participants: [...clients.keys()].filter(x => x !== id) }));
+  io.emit("users", Object.values(users));
 
-  ws.on("message", (msg) => {
-    let data;
-    try {
-      data = JSON.parse(msg);
-    } catch (e) {
-      return;
-    }
-
-    switch (data.type) {
-      case "join":
-        broadcastExcept(id, { type: "existing-participants", participants: [...clients.keys()] });
-        break;
-      case "offer":
-      case "answer":
-      case "ice-candidate":
-        if (clients.has(data.to)) {
-          clients.get(data.to).send(JSON.stringify({ ...data, from: id }));
-        }
-        break;
-      case "leave":
-        broadcastExcept(id, { type: "participant-left", id });
-        break;
-    }
+  socket.on("setName", (name) => {
+    users[socket.id] = name;
+    io.emit("users", Object.values(users));
   });
 
-  ws.on("close", () => {
-    clients.delete(id);
-    broadcastExcept(id, { type: "participant-left", id });
-    console.log(`Client ${id} disconnected`);
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+    io.emit("users", Object.values(users));
   });
 });
 
-function broadcastExcept(senderId, message) {
-  const msg = JSON.stringify(message);
-  for (const [id, ws] of clients.entries()) {
-    if (id !== senderId && ws.readyState === WebSocket.OPEN) ws.send(msg);
-  }
-}
-
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`✅ Video chat server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
