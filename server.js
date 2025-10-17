a497ae8223197c3302905c3bf19605e9882d5627
knovaonfortnite-boop <knovaonfortnite@gmail.com>
2025-10-16 22:33:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import http from "http";
+import { createServer } from "http";
 import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -8,38 +8,36 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
 const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const users = {}; // { socketId: username }
+const users = {};
 
 io.on("connection", (socket) => {
-  console.log("New user connected:", socket.id);
-
   socket.on("join", (username) => {
     users[socket.id] = username;
-    io.emit("update-users", users);
+    io.emit("users", users);
   });
 
-  socket.on("call-user", ({ to, offer }) => {
-    io.to(to).emit("incoming-call", { from: socket.id, offer, username: users[socket.id] });
+  socket.on("call-user", (targetId) => {
+    io.to(targetId).emit("incoming-call", socket.id);
   });
 
-  socket.on("answer-call", ({ to, answer }) => {
-    io.to(to).emit("call-answered", { from: socket.id, answer });
+  socket.on("offer", (data) => {
+    io.to(data.target).emit("offer", { sdp: data.sdp, from: socket.id });
   });
 
-  socket.on("ice-candidate", ({ to, candidate }) => {
-    io.to(to).emit("ice-candidate", { from: socket.id, candidate });
+  socket.on("answer", (data) => {
+    io.to(data.target).emit("answer", { sdp: data.sdp, from: socket.id });
   });
 
   socket.on("disconnect", () => {
     delete users[socket.id];
-    io.emit("update-users", users);
+    io.emit("users", users);
   });
 });
 
-const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
