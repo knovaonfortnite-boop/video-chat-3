e530ad4b2bd0c918cbe76f3a776f7bc891d21595
knovaonfortnite-boop <knovaonfortnite@gmail.com>
2025-10-16 22:09:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
@@ -9,23 +13,33 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let users = {};
+const users = {}; // { socketId: username }
 
 io.on("connection", (socket) => {
-  users[socket.id] = "Anonymous";
+  console.log("New user connected:", socket.id);
 
-  io.emit("users", Object.values(users));
+  socket.on("join", (username) => {
+    users[socket.id] = username;
+    io.emit("update-users", users);
+  });
 
-  socket.on("setName", (name) => {
-    users[socket.id] = name;
-    io.emit("users", Object.values(users));
+  socket.on("call-user", ({ to, offer }) => {
+    io.to(to).emit("incoming-call", { from: socket.id, offer, username: users[socket.id] });
+  });
+
+  socket.on("answer-call", ({ to, answer }) => {
+    io.to(to).emit("call-answered", { from: socket.id, answer });
+  });
+
+  socket.on("ice-candidate", ({ to, candidate }) => {
+    io.to(to).emit("ice-candidate", { from: socket.id, candidate });
   });
 
   socket.on("disconnect", () => {
     delete users[socket.id];
-    io.emit("users", Object.values(users));
+    io.emit("update-users", users);
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
