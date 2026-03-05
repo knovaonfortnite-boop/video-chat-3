6820e4958527a8541bfb5b5850ffd2c2f2e338e1
knovaonfortnite-boop <knovaonfortnite@gmail.com>
2026-03-04 23:10:02 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 
@@ -8,56 +6,41 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static('public')); // serve index.html & app.js
+app.use(express.static('public'));
 
-// In-memory storage
-const users = {}; // { username: { firstName, lastName, passwordHash } }
-const channels = {}; // { channelId: { messages: [{user, text}] } }
+let messages = []; // temporary public messages
+let users = {};    // { nickname: socket.id }
 
-// Default channel
-const defaultChannelId = 'general';
-channels[defaultChannelId] = { messages: [] };
-
-// Helpers
-function generateId() {
-  return Math.random().toString(36).substr(2, 9);
-}
-
-// Routes
-app.post('/register', async (req, res) => {
-  const { firstName, lastName, password } = req.body;
-  const username = `${firstName}${lastName}`.toLowerCase();
-  if (users[username]) return res.status(400).send('Username taken');
-  const hashedPw = await bcrypt.hash(password, 10);
-  users[username] = { firstName, lastName, passwordHash: hashedPw };
-  res.status(201).send('User registered');
-});
-
-app.post('/login', async (req, res) => {
-  const { firstName, lastName, password } = req.body;
-  const username = `${firstName}${lastName}`.toLowerCase();
-  const user = users[username];
-  if (!user || !(await bcrypt.compare(password, user.passwordHash))) {
-    return res.status(400).send('Invalid credentials');
-  }
-  res.json({ username });
-});
-
-// Socket.io
 io.on('connection', socket => {
   console.log('User connected');
 
-  socket.on('joinChannel', channelId => socket.join(channelId));
-
-  socket.on('sendMessage', ({ channelId, user, text }) => {
-    if (!channels[channelId]) channels[channelId] = { messages: [] };
-    channels[channelId].messages.push({ user, text });
-    io.to(channelId).emit('message', { user, text });
+  // Set nickname
+  socket.on('setNickname', nickname => {
+    users[nickname] = socket.id;
+    socket.nickname = nickname;
+    // Send current public messages
+    socket.emit('allMessages', messages);
   });
 
-  socket.on('disconnect', () => console.log('User disconnected'));
+  // Public message
+  socket.on('sendMessage', text => {
+    const msg = { user: socket.nickname, text };
+    messages.push(msg);
+    io.emit('message', msg);
+  });
+
+  // Private message
+  socket.on('sendPrivate', ({ target, text }) => {
+    const targetSocketId = users[target];
+    if (targetSocketId) {
+      io.to(targetSocketId).emit('privateMessage', { from: socket.nickname, text });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.nickname} disconnected`);
+    delete users[socket.nickname];
+  });
 });
 
-server.listen(3000, () => console.log('Server running on 3000'));
+server.listen(process.env.PORT || 3000, () => console.log('Server running'));
